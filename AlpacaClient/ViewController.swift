21dcee44c30e7807dc70b0d7c4cbe48fa34bc021
Sergey Gernyak <serhii.herniak@thredup.com>
2023-02-06 16:01:38 +0200

    Initial Commit

---
+++ AlpacaClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlpacaClient
+//
+//  Created by Sergey Gernyak on 06.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
