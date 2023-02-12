ca671b24c02b3d67f4dfc0e5146e0d1ae207e75a
Sergey Gernyak <serhii.herniak@thredup.com>
2023-02-12 18:30:28 +0200

    Get rid of Main storyboard

--- AlpacaClient/ViewController.swift
+++ AlpacaClient/ViewController.swift
@@ -42,6 +42,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         
         setupView()
         
