27f21cb47b2e7f8cfba0b872a556b253705f791f
Sergey Gernyak <serhii.herniak@thredup.com>
2023-02-19 16:35:39 +0200

    Add identifying animation by key

--- AlpacaClient/Extensions/UIView + Extension.swift
+++ AlpacaClient/Extensions/UIView + Extension.swift
@@ -8,18 +8,22 @@
 import Foundation
 import UIKit
 
+private let rotate360DegreesAnimationKey = "rotate360Degrees"
+
 extension UIView {
     func rotate360Degrees(duration: CFTimeInterval = 3) {
+        stopRotate360Degrees()
+        
         let rotateAnimation = CABasicAnimation(keyPath: "transform.rotation")
         rotateAnimation.fromValue = 0.0
         rotateAnimation.toValue = CGFloat(Double.pi * 2)
         rotateAnimation.isRemovedOnCompletion = false
         rotateAnimation.duration = duration
         rotateAnimation.repeatCount=Float.infinity
-        layer.add(rotateAnimation, forKey: nil)
+        layer.add(rotateAnimation, forKey: rotate360DegreesAnimationKey)
     }
     
     func stopRotate360Degrees() {
-        layer.removeAllAnimations()
+        layer.removeAnimation(forKey: rotate360DegreesAnimationKey)
     }
 }
