0c93e342c2d00a5236ad0f1a4cf5da678197fb70
Sergey Gernyak <serhii.herniak@thredup.com>
2023-02-21 12:10:17 +0200

    Add logging API request/response

--- AlpacaClient/NetworkManagement/HTTPDownloader.swift
+++ AlpacaClient/NetworkManagement/HTTPDownloader.swift
@@ -19,9 +19,13 @@ enum HTTPDownloaderError: Error {
 
 extension URLSession: HTTPDownloader {
     func httpData(for urlRequest: URLRequest) async throws -> Data {
+        Log.API(request: urlRequest)
+        
         guard let (data, response) = try await self.data(for: urlRequest, delegate: nil) as? (Data, HTTPURLResponse) else {
             throw HTTPDownloaderError.httpError(response: nil)
         }
+        
+        Log.API(response: response, object: data)
         
         guard validStatus.contains(response.statusCode) else {
             throw HTTPDownloaderError.httpError(response: data)
--- AlpacaClient/Utilities/Log.swift
+++ AlpacaClient/Utilities/Log.swift
@@ -50,6 +50,62 @@ class Log {
     class func severe(_ object: Any) {
         print("\(Date.now.toString()) \(EventType.severe.rawValue)[\(object)")
     }
+    
+    class func API(request: URLRequest) {
+        var mutableString = "\n"
+
+        if let httpMethod = request.httpMethod {
+            mutableString.append("➡️ \(httpMethod) ")
+        }
+        if let url = request.url {
+            mutableString.append("\(url)")
+        }
+        if let allHTTPHeaderFields = request.allHTTPHeaderFields {
+            mutableString.append("\n📜 Header Fields:")
+            if allHTTPHeaderFields.count > 0 {
+                for (key, value) in allHTTPHeaderFields {
+                    mutableString.append("\n\(key): \(value)")
+                }
+            } else {
+                mutableString.append("\n<no-headers-specified>")
+            }
+        }
+        if let httpBody = request.httpBody {
+            mutableString.append("\n📦 Body: \(String(decoding: httpBody, as: UTF8.self))")
+        } else {
+            mutableString.append("\n📦 Body: <no-data>")
+        }
+
+        print("\(Date().toString()) \(EventType.api.rawValue) \(mutableString)")
+    }
+    
+    class func API(response: URLResponse, object: Data?) {
+        guard let httpUrlResponse = response as? HTTPURLResponse else { return }
+        let statusCode = httpUrlResponse.statusCode
+
+        var mutableString = "\n"
+
+        mutableString.append("⬅️ \(statusCode) ")
+
+        if let url = response.url {
+            mutableString.append("\(url)")
+        }
+        mutableString.append("\n📜 Header Fields:")
+        if httpUrlResponse.allHeaderFields.count > 0 {
+            for (key, value) in httpUrlResponse.allHeaderFields {
+                mutableString.append("\n\(key): \(value)")
+            }
+        } else {
+            mutableString.append("\n<no-headers-specified>")
+        }
+        if let object = object {
+            mutableString.append("\n📦 Body: \(String(decoding: object, as: UTF8.self))")
+        } else {
+            mutableString.append("\n📦 Body: <no-data>")
+        }
+
+        print("\(Date().toString()) \(EventType.api.rawValue) \(mutableString)")
+    }
 }
 
 private extension Date {
